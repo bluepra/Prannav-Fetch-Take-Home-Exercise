bc1b7357e493ff75447196af5e4a0c85fb14e8e7
bluepra <a.prannav@gmail.com>
2024-01-29 23:12:06 -0500

    Added python function documentation to all functions

--- process_receipts.py
+++ process_receipts.py
@@ -3,6 +3,14 @@ import math
 # v----------- RULES FOR POINTS -----------v
 
 def retailer_name_alphanum_pts(retailer_name: str) -> int:
+    """
+    This function tallies up 1 point for every alpha-numeric character
+    in the retailer name
+
+    :param retailer_name: Name of the retailer
+    :return: Points collected for this rule
+    """
+
     pts = 0
 
     # For each letter of the retailer num, add a point
@@ -14,6 +22,13 @@ def retailer_name_alphanum_pts(retailer_name: str) -> int:
     return pts
 
 def total_round_num_pts(total: float) -> int:
+    """
+    This function tallies up 50 points if the total is a round dollar amount with no cents
+
+    :param total: The receipt total in dollars and cents
+    :return: Points collected for this rule
+    """
+
     pts = 0
 
     # If the total is a whole number, add 50 pts
@@ -23,6 +38,13 @@ def total_round_num_pts(total: float) -> int:
     return pts
 
 def total_multiple_of_pts(total: float) -> int:
+    """
+    This function tallies up 25 points if the total is a multiple of .25
+
+    :param total: The receipt total in dollars and cents
+    :return: Points collected for this rule
+    """
+
     pts = 0
 
     # If total is a multiple of .25, add 25 pts
@@ -32,6 +54,13 @@ def total_multiple_of_pts(total: float) -> int:
     return pts
 
 def every_two_items_pts(num_items: int) -> int:
+    """
+    This function tallies up 5 points for every two items in the receipt
+
+    :param num_items: The number of items in the receipt
+    :return: Points collected for this rule
+    """
+
     pts = 0
 
     pts_per_two_items = 5
@@ -41,6 +70,18 @@ def every_two_items_pts(num_items: int) -> int:
     return pts
 
 def item_descriptions_trimmed_pts(items: list) -> int:
+    """
+    This function tallies up points if the item descriptions follow a rule.
+
+    The rule for each item is:
+    If the trimmed length of the item description is a multiple of 3, 
+    multiply the item price by 0.2 and round up to the nearest integer. 
+    The result is the number of points earned for that item.
+
+    :param items: The list of items in the receipt
+    :return: Points collected for this rule
+    """
+
     pts = 0
 
     multiplier = .2
@@ -57,6 +98,13 @@ def item_descriptions_trimmed_pts(items: list) -> int:
     return pts
 
 def purchase_date_odd_pts(purchase_date: str) -> str:
+    """
+    This function tallies up 6 points if the purchase date is odd
+
+    :param purchase_date: The string format ('yyyy-mm-dd') of the date of purchase
+    :return: Points collected for this rule
+    """
+
     pts = 0
 
     # Since purchase_date is a string of format yyyy-mm-dd
@@ -70,6 +118,13 @@ def purchase_date_odd_pts(purchase_date: str) -> str:
     return pts
 
 def time_of_purchase_pts(purchase_time: str) -> str:
+    """
+    This function tallies up 10 points if the purchase time between 2 PM and 4 PM
+
+    :param purchase_time: The string format ('hh:mm') of the time of purchase following a 24 hour clock
+    :return: Points collected for this rule
+    """
+
     pts = 0
 
     # Since purchase_time is a string of format hh:mm
@@ -88,7 +143,15 @@ def time_of_purchase_pts(purchase_time: str) -> str:
 # ^----------- RULES FOR POINTS -----------^
 
 # This function calculates the correct number of reward points for the given receipt
-def calculate_reward_points(receipt: dict) -> int: 
+def calculate_reward_points(receipt: dict) -> int:
+    """
+    This function takes the receipt and awards all possible points based
+    on the rules defined.
+
+    :param receipt: The receipt
+    :return: Total points rewarded to the input receipt
+    """
+
     # Unpack the receipt for convenience
     retailer = receipt['retailer']
     purchase_date = receipt['purchaseDate']
