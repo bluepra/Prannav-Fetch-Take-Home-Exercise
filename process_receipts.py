095e361e7bcd5de97bcae350a872812cbf4f70a4
bluepra <a.prannav@gmail.com>
2024-02-03 11:17:10 -0500

    Fixed the pts to be awarded after 2 PM and before 4 PM, and moved validation receipt code to another file

--- process_receipts.py
+++ process_receipts.py
@@ -1,119 +1,4 @@
 import math
-import re
-
-
-# Ideally we would use logging instead of print statements
-
-def all_items_valid(items: list) -> bool:
-    """
-    This function checks if the items list is valid
-
-    :param items: items of a receipt
-    :return: True if all items are valid, false otherwise
-    """
-    
-    for item in items:
-        if not isinstance(item, dict):
-            print('An item not a dict - invalid receipt')
-            return False
-    
-        if 'shortDescription' not in item or 'price' not in item:
-            print('An item is missing a required key - invalid receipt')
-            return False
-        
-        # shortDescription valid
-        shortDescription = item['shortDescription']
-        if not isinstance(shortDescription, str) or (not re.match("^[\\w\\s\\-]+$", shortDescription)):
-            print('An item\'s shortDescription is invalid - invalid receipt')
-            return False
-        
-        # price valid
-        price = item['price']
-        if not isinstance(price, str) or (not re.match("^\\d+\\.\\d{2}$", price)):
-            print('An item\'s price is invalid - invalid receipt')
-            return False
-        
-    # All items in the items list are good
-    return True
-    
-
-
-def is_valid_receipt(receipt: dict) -> bool:
-    """
-    This function checks if the receipt is valid
-
-    :param receipt: the receipt we want to validate
-    :return: True if receipt is valid, false otherwise
-    """
-
-    # Receipt must be a dictionary
-    if not isinstance(receipt, dict):
-        print('The receipt is not a dictionary - invalid receipt')
-        return False
-    
-    # All required keys must be present
-    required_keys = ['retailer', 'purchaseDate', 'purchaseTime', 'items', 'total']
-
-    for key in required_keys:
-        if key not in receipt:
-            print(f'The receipt is missing key {key} - invalid receipt')
-            return False
-    
-    # Retailer name valid
-    retailer = receipt['retailer']
-
-    # IMPORTANT: For some reason this pattern is failing to match M&M Corner Market 
-    # which is a valid retailer name
-    if not isinstance(retailer, str) or (not re.match("^[\\w\\s\\-]+$", retailer)):
-        print(f'A receipts\'s retailer ({retailer}) is invalid - invalid receipt')
-        return False
-    
-    # Date must be valid -> Ideally need add a regex pattern check for yyyy-mm-dd
-    purchaseDate = receipt['purchaseDate']
-    dateComponents = purchaseDate.split('-')
-    if not isinstance(purchaseDate, str) or purchaseDate == '' or len(dateComponents) != 3:
-        print('A receipts\'s purchaseDate is invalid - invalid receipt')
-        return False
-    
-    for i in dateComponents:
-        if not i.isdigit():
-            print('A receipts\'s purchaseDate is invalid - invalid receipt')
-            return False
-        
-    
-    # Time must be valid -> Ideally need add a regex pattern check for hh:mm
-    # Asssuming no spaces allowed in time
-    purchaseTime = receipt['purchaseTime']
-    timeComponents = purchaseTime.split(':')
-    if not isinstance(purchaseTime, str) or purchaseTime == '' or len(timeComponents) != 2:
-        print('A receipts\'s purchaseTime is invalid - invalid receipt')
-        return False
-    
-    for i in timeComponents:
-        if not i.isdigit():
-            print('A receipts\'s purchaseTime is invalid - invalid receipt')
-            return False
-    
-    # Valid items -> Must have at least 1 item
-    items = receipt['items']
-    if not isinstance(items, list) or len(items) < 1:
-        print(retailer, )
-        print('A receipts\'s items list is invalid - invalid receipt')
-        return False
-    
-    if not all_items_valid(items):
-        return False
-
-    
-    # Valid total
-    total = receipt['total']
-    if not isinstance(total, str) or (not re.match("^\\d+\\.\\d{2}$", total)):
-        print('A receipts\'s total is invalid - invalid receipt')
-        return False
-    
-    # All good in the receipt
-    return True
-
 
 
 # v----------- RULES FOR POINTS -----------v
@@ -235,7 +120,7 @@ def purchase_date_odd_pts(purchase_date: str) -> str:
 
 def time_of_purchase_pts(purchase_time: str) -> str:
     """
-    This function tallies up 10 points if the purchase time between 2 PM and 4 PM
+    This function tallies up 10 points if the purchase time after 2 PM and before 4 PM
 
     :param purchase_time: The string format ('hh:mm') of the time of purchase following a 24 hour clock
     :return: Points collected for this rule
@@ -249,9 +134,10 @@ def time_of_purchase_pts(purchase_time: str) -> str:
     mins = int(purchase_time.split(':')[-1])
 
     # If the hour falls between 14 and 16, then add to pts
-    if hour >= 14 and hour <= 16:
-        if hour != 16 or (hour == 16 and mins == 0):
-            pts += 10
+    # If the time says 14:00 it means it has to be a little over 2 PM
+    # If the time says 16:00 it means it has to be a little over 4 PM
+    if hour >= 14 and hour <= 15:
+        pts += 10
 
     return pts
 
@@ -275,9 +161,7 @@ def calculate_reward_points(receipt: dict) -> int:
     items = receipt['items']
     total = float(receipt['total'])
 
-    # Ideally we wouldn't throw an error like this,
-    # We would check for receipt errors, but because of M&M Corner Market regex pattern
-    # match failure, I have not used the is_valid_receipt() function
+
     if len(items) < 1:
         raise ValueError('You need at least 1 item in your items list')
 
